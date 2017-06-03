2425c1eddf63b495884bbcf32067f6ba3c9cc5ed
Szymon <miks.szymon@gmail.com>
2017-06-03 20:17:40 +0200

    add some js file with my private.js for menu

--- js/private.js
+++ js/private.js
@@ -8,3 +8,35 @@ $(document).ready(function($) {
         }
     });
 });
+
+
+jQuery(document).ready(function( $ ) {
+    $("#menu").mmenu({
+        "extensions": [
+            "pagedim-black"
+        ],
+        "navbars": [
+            {
+                "position": "top",
+                "content": [
+                    "searchfield"
+                ]
+            },
+            {
+                "position": "top"
+            },
+        ]
+    });
+});
+
+
+$(document).ready(function(){
+    $('.hamburger').click(function(){
+        $(this).addClass('is-active');
+    })
+    $('#mm-blocker').click(function(){
+        if($('.hamburger').hasClass('is-active')) {
+            $('.hamburger').removeClass('is-active');
+        }
+    })
+})
